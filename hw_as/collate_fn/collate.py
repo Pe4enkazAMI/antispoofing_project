943dc8d12bc1c1fd950c189adbd8137e588dc240
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-12-07 19:54:12 +0300

    onebatch ready

--- hw_as/collate_fn/collate.py
+++ hw_as/collate_fn/collate.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def collate_fn(dataset_items: List[dict]) -> dict:
-    audios = pad_sequence([items["audio"] for items in dataset_items], batch_first=True)
+    audios = pad_sequence([items["audio"].reshape(-1) for items in dataset_items], batch_first=True)
     targets = torch.tensor([items["target"] for items in dataset_items])
     return {
         "audio": audios,
