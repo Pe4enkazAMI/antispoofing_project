666553b6633365137b8ae6e31a3ff3bb2657bfdc
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-12-13 15:18:41 +0300

    lolka

--- hw_as/model/RawNet2.py
+++ hw_as/model/RawNet2.py
@@ -5,6 +5,14 @@ import torch.nn.functional as F
 import torch
 import numpy as np
 
+class NormLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        x_norm = x.norm(p=2,dim=1, keepdim=True) / 10.
+        x = torch.div(x, x_norm)
+        return x
+
 
 class RawNet2(nn.Module):
     def __init__(self, 
@@ -22,13 +30,6 @@ class RawNet2(nn.Module):
             kernel_size = ks_conv_sinc
             )
 
-        # self.first_bn = nn.BatchNorm1d(num_features = filts[0])
-        # self.lrelu_keras = nn.LeakyReLU(negative_slope = 0.3)
-        
-        # self.block0 = nn.Sequential(ResBlock(emb_dims = filts[1], first = True))
-        # self.block1 = nn.Sequential(ResBlock(emb_dims = filts[1]))
- 
-        # self.block2 = nn.Sequential(ResBlock(emb_dims = filts[2]))
         self.prejump = nn.Sequential(
             nn.BatchNorm1d(num_features = filts[0]),
             nn.LeakyReLU(negative_slope = 0.3),
@@ -45,12 +46,7 @@ class RawNet2(nn.Module):
             nn.BatchNorm1d(num_features = filts[2][-1]),
             nn.LeakyReLU(negative_slope = 0.3)
         )
-        # self.block3 = nn.Sequential(ResBlock(emb_dims = filts[2]))
-        # self.block4 = nn.Sequential(ResBlock(emb_dims = filts[2]))
-        # self.block5 = nn.Sequential(ResBlock(emb_dims = filts[2]))
-        # self.avgpool = nn.AdaptiveAvgPool1d(1)
 
-        # self.bn_before_gru = nn.BatchNorm1d(num_features = filts[2][-1])
         self.gru = nn.GRU(input_size = filts[2][-1],
             hidden_size = gru_node,
             num_layers = num_gru_layers,
@@ -58,6 +54,8 @@ class RawNet2(nn.Module):
         
         self.fc1_gru = nn.Linear(in_features = gru_node,
             out_features = num_fc_feats)
+        
+        self.norm_layer = NormLayer()
         self.fc2_gru = nn.Linear(in_features = num_fc_feats,
             out_features = num_classes,
             bias = True)
@@ -68,27 +66,15 @@ class RawNet2(nn.Module):
         len_seq = x.shape[1]
         x = x.view(nb_samp, 1,len_seq)
         x = F.max_pool1d(self.first_conv(x), 3)
-        # x = self.first_bn(x)
-        # x = self.lrelu_keras(x)
-        
-        # x = self.block0(x)
-        # x = self.block1(x)
 
-        # x = self.block2(x)
         x = self.prejump(x)
-        # x = self.block3(x)
-        # x = self.block4(x)
-        # x = self.block5(x)
 
-        # x = self.bn_before_gru(x)
-        # x = self.lrelu_keras(x)
         x = self.afjump(x)
         x = x.permute(0, 2, 1)  #(batch, filt, time) >> (batch, time, filt)
         x, _ = self.gru(x)
         x = x[:,-1,:]
         code = self.fc1_gru(x)
-        code_norm = code.norm(p=2,dim=1, keepdim=True) / 10.
-        code = torch.div(code, code_norm)
+        code = self.norm_layer(code)
         out = self.fc2_gru(code)
         return {"logits": out}
     
